c8edbc2ff00b3bfcf60a1a9162d29b9a6e56b308
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-02-22 01:03:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+
+app.use(express.json());
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
 
+
+app.post('/api/products', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hello from Node API Server Updated')
 });
+
 
 mongoose.connect('mongodb+srv://bridgitkm:0724257014@backenddb.g4qorh2.mongodb.net/Node-API?retryWrites=true&w=majority')
         .then(() => {
