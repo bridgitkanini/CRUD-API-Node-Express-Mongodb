9d5b76d4e6ff79d8305036a927ef96b1b5c4d4ea
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-02-22 01:13:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('/models/product.model.js')
 const app = express();
 
 
@@ -11,9 +12,13 @@ app.listen(3000, () => {
 });
 
 
-app.post('/api/products', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 
