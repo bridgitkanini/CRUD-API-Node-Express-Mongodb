bb9a43084132a207a57782e3d577273d7ddb07bf
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-02-23 00:05:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
 const app = express();
 
+//Middleware.
 app.use(express.json());
+app.use(express.urlencoded({ extended: false })); //Middleware configuration to add new products through Form URL Encoded on Insomnia.
+
+//Routes.
+app.use("/api/products", productRoute);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-//Create products; Post, Add a Product folder on insomnia. Body, JSON.
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//Read all products; Get All Products folder and Get request on insomnia.
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//Read one product; Yes, Get.
-app.get("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-
-
-//Update a product; Put.
-app.put("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const product = await Product.findByIdAndUpdate(id, req.body);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found." });
-    }
-
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//Delete a product
-app.delete("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const product = await Product.findByIdAndDelete(id, req.body);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found." });
-    }
-
-    res.status(200).json({ message: "Product deleted successfully." });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-})
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
