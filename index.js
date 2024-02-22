eef1fdf62ee90c6f5022d11d5a3c527dc804b9e9
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-02-22 22:00:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,93 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Product = require('/models/product.model.js')
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
-
 
 app.use(express.json());
 
-
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+  console.log("Server is running on port 3000");
+});
+
+//Create products; Post, Add a Product folder on insomnia. Body, JSON.
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Read all products; Get All Products folder and Get request on insomnia.
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Read one product; Yes, Get.
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
+
+//Update a product; Put.
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found." });
     }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
+//Delete a product
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-app.get('/', (req, res) => {
-    res.send('Hello from Node API Server Updated')
+    const product = await Product.findByIdAndDelete(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found." });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully." });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
 });
 
-
-mongoose.connect('mongodb+srv://bridgitkm:0724257014@backenddb.g4qorh2.mongodb.net/Node-API?retryWrites=true&w=majority')
-        .then(() => {
-            console.log('Connected to database!')
-        })
-        .catch(() => {
-            console.log('Connection failed!')
-        })
+mongoose
+  .connect(
+    "mongodb+srv://bridgitkm:0724257014@backenddb.g4qorh2.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
